e18968d2aae14057adfd06d340f5097a12b90f5e
Varpikhovsky Svyatoslav <varpihovsky@gmail.com>
2017-09-16 11:24:34 +0300

    Finish prototype session wrapper

--- src/Session.php
+++ src/Session.php
@@ -3,7 +3,93 @@ namespace Geega\Session;
 
 
 class Session {
-  public function echo ($msg) {
-    echo $msg.PHP_EOL;
-  }
+
+    /**
+     * Start session
+     * @return bool
+     */
+    public static function start()
+    {
+        if(!headers_sent() && !session_id()){
+            if(session_start()){
+                session_regenerate_id();
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * Set key value to session
+     * @param $key
+     * @param $value
+     */
+    public static function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+
+    /**
+     * Check has session value by key
+     * @param $key
+     * @return bool
+     */
+    public static function has($key)
+    {
+        return (bool) static::get($key);
+    }
+
+
+    /**
+     * Get session value by key
+     * @param $key
+     * @return null
+     */
+    public static function get($key)
+    {
+        return (isset($_SESSION[$key])) ? $_SESSION[$key] : null;
+    }
+
+
+    /**
+     * Delete by key
+     * @param $key
+     * @return bool
+     */
+    public static function del($key)
+    {
+        if(isset($_SESSION[$key])){
+            unset($_SESSION[$key]);
+            return false;
+        }
+
+    }
+
+
+    /**
+     * Destroy session
+     */
+    public static function destroy()
+    {
+        if(isset($_SESSION)){
+            session_destroy();
+        }
+    }
+
+
+    /**
+     * @throws \Exception
+     */
+    public static function dumpPrint()
+    {
+        if(isset($_SESSION))
+        {
+            print_r($_SESSION);
+            return ;
+        }
+
+        throw new \Exception("Session is not initialized");
+    }
 }
